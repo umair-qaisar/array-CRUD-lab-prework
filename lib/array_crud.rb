bc26df2da92e93925d708a13e90b7af69ec8386c
Umair Qaisar <qaisaru@icloud.com>
2018-09-12 06:48:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,11 +26,11 @@ def remove_element_from_start_of_array(array)
 end
 
 ef add_element_to_end_of_array(array, element)
-  array.push"#{element}"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift"#{element}"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
